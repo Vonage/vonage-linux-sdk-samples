83d796922d0b13300d150fe7ed7af3181c23e0af
aos-svg <artur.osinski@vonage.com>
2026-03-12 08:26:32 +0100

    Move scripts

---
+++ scripts/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+unset CDPATH
+
+SCRIPT_PATH=${SCRIPT_PATH:-$(cd `dirname ${BASH_SOURCE[0]}`; pwd)}
+
+SAMPLES=(Basic-Video-Chat Publisher-Only Signaling Configurable-TURN-Servers RTSP-Stream-Video-Capturer Custom-Audio-Device Custom-Video-Capturer Background-Substractor-Video-Capturer)
+for sample in ${SAMPLES[@]}; do
+  rm -rf ${SCRIPT_PATH}/../${sample}/build
+  mkdir ${SCRIPT_PATH}/../${sample}/build
+  cp ${SCRIPT_PATH}/../common/src/config-sample.h ${SCRIPT_PATH}/../${sample}/config.h
+  pushd ${SCRIPT_PATH}/../${sample}/build
+  CC=clang CXX=clang++ cmake -DCI=ON ..
+  ret=$?
+  if [ ${ret} -ne 0 ]; then
+    echo "Cannot build ${sample} correctly. 'build.sh' script failed. Abort."
+    popd
+    exit 255
+  fi
+  make
+  ret=$?
+  if [ ${ret} -ne 0 ]; then
+    echo "Cannot build ${sample} correctly. 'build.sh' script failed. Abort."
+    popd
+    exit 255
+  fi
+  popd
+done
+
+exit 0
